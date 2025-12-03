c84a1e646502ec41a3c620e3155726cbbb919a71
DevPabloCaseJsX <140985399+DevPabloCaseJsX@users.noreply.github.com>
2025-12-02 21:42:26 -0400

    Change response message in root route

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hola Mundo desde GitHub Actions + Docker + Render!");
+    res.send("Hola Mundo desde GitHub UP");
 });
 
 const PORT = process.env.PORT || 8080;
